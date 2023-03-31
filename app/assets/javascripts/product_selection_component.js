fc6ed643d1b401931c8fa3e47fba84d1237f0f77
KitokoInHerTruck <coquerel.isabelle@hotmail.fr>
2023-03-31 16:22:32 +0200

    fixed event error currentstore

--- app/assets/javascripts/product_selection_component.js
+++ app/assets/javascripts/product_selection_component.js
@@ -1,62 +1,126 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const radios = document.querySelectorAll("[data-js='variant-radio']");
-  const thumbnailsLinks = document.querySelectorAll(
-    "[data-js='product-thumbnail'] a, [data-js='variant-thumbnail'] a"
-  );
-  const productImage = document.querySelector("[data-js='product-main-image']");
-  const variantsThumbnails = document.querySelectorAll(
-    "[data-js='variant-thumbnail']"
-  );
-
-  if (radios.length > 0) {
-    const selectedRadio = document.querySelector(
-      "[data-js='variant-radio'][checked='checked']"
-    );
-    updateVariantPrice(selectedRadio);
-    updateVariantImages(selectedRadio.value);
+document.addEventListener("DOMContentLoaded", function () {
+  const optionTypeSelector = document.querySelectorAll(".selection-items");
+  for (var i = 0; i < optionTypeSelector.length; i++) {
+    optionTypeSelector[i].addEventListener("click", onSelection);
   }
-
-  radios.forEach((radio) => {
-    radio.addEventListener("click", () => {
-      updateVariantPrice(radio);
-      updateVariantImages(radio.value);
-    });
-  });
-
-  thumbnailsLinks.forEach((thumbnailLink) => {
-    thumbnailLink.addEventListener("click", (event) => {
-      event.preventDefault();
-      updateProductImage(thumbnailLink.href);
-    });
-  });
-
-  function updateVariantPrice(variant) {
-    const variantPrice = variant.dataset.jsPrice;
-    if (variantPrice) {
-      document.querySelector("[data-js='price']").innerHTML = variantPrice;
-    }
-  }
-
-  function updateVariantImages(variantId) {
-    selector = "[data-js='variant-thumbnail'][data-js-id='" + variantId + "']";
-    variantsThumbnailsToDisplay = document.querySelectorAll(selector);
-
-    variantsThumbnails.forEach((thumbnail) => {
-      thumbnail.style.display = "none";
-    });
-
-    variantsThumbnailsToDisplay.forEach((thumbnail) => {
-      thumbnail.style.display = "list-item";
-    });
-
-    if (variantsThumbnailsToDisplay.length) {
-      variantFirstImage =
-        variantsThumbnailsToDisplay[0].querySelector("a").href;
-      updateProductImage(variantFirstImage);
-    }
-  }
-
-  function updateProductImage(imageSrc) {
-    productImage.src = imageSrc;
-  }
+  selectFirstVariant();
 });
+
+function selectFirstVariant() {
+  const firstVariant = document.querySelector("[data-option-index]");
+  if (firstVariant) {
+    setTimeout(() => {
+      firstVariant.click();
+    }, 1);
+  }
+}
+
+function onSelection(event) {
+  document.getElementById(`selected-${event.target.name}`).innerText =
+    event.target.dataset.presentation;
+
+  const optionIndex = event.target.attributes["data-option-index"].value;
+  const nextType = document.querySelector(
+    `[data-option-index="${parseInt(optionIndex, 10) + 1}"]`
+  );
+  if (nextType) {
+    updateOptions(nextType.name, optionIndex);
+  }
+  selectVariant();
+}
+
+function updateStockView(status) {
+  if (status) {
+    this.stockIndicatorTarget.classList.add("d-hide");
+    this.cartButtonTarget.disabled = false;
+  } else {
+    this.stockIndicatorTarget.classList.remove("d-hide");
+    this.cartButtonTarget.disabled = true;
+  }
+}
+
+function updateOptions(nextTypeName, optionIndex) {
+  const nextOptionValues = this.nextOptionValues(optionIndex);
+
+  let firstRadio = null;
+  const allNextOptions = [
+    ...document.querySelectorAll(`[name="${nextTypeName}"]`),
+  ];
+  allNextOptions.forEach((radio) => {
+    if (!nextOptionValues.includes(parseInt(radio.value, 10))) {
+      radio.disabled = true;
+      radio.parentElement.classList.add("d-hide");
+    } else {
+      radio.disabled = false;
+      radio.parentElement.classList.remove("d-hide");
+      if (!firstRadio) {
+        firstRadio = radio;
+      }
+    }
+  });
+
+  const nextSelectedRadio = document.querySelector(
+    `[name="${nextTypeName}"]:checked`
+  );
+  if (nextSelectedRadio.disabled) {
+    firstRadio.click();
+  } else {
+    nextSelectedRadio.click();
+  }
+}
+
+function nextOptionValues(optionIndex) {
+  const values = [];
+  const variantOptionsTargets = document.querySelectorAll(
+    ".product-variants__list > li > input"
+  );
+  variantOptionsTargets.forEach((option) => {
+    const optionValueIds = JSON.parse(
+      option.attributes["data-option-value-ids"].value
+    );
+    const selectedOptionIds = this.currentSelection();
+    let matched = true;
+    for (let i = 0; i <= optionIndex; i += 1) {
+      if (optionValueIds[i] !== selectedOptionIds[i]) {
+        matched = false;
+        break;
+      }
+    }
+    if (matched) {
+      values.push(optionValueIds[parseInt(optionIndex, 10) + 1]);
+    }
+  });
+  return values;
+}
+
+function updateView(variant) {
+  document.querySelector("#product-price").innerHTML = variant.dataset.price;
+}
+
+function selectVariant() {
+  this.variant = document.querySelector(
+    `[data-option-value-ids="${JSON.stringify(this.currentSelection())}"]`
+  );
+  if (this.variant) {
+    this.variant.click();
+    this.updateView(this.variant);
+  } else {
+    this.priceTarget.innerText =
+      "Not found, please select all optionTypeSelector";
+  }
+}
+
+function currentSelection() {
+  let i = 0;
+  const selectionArr = [];
+  while (document.querySelector(`[data-option-index="${i}"]`)) {
+    selectionArr.push(
+      parseInt(
+        document.querySelector(`[data-option-index="${i}"]:checked`).value,
+        10
+      )
+    );
+    i += 1;
+  }
+  return selectionArr;
+}
--- app/controllers/solidus_paypal_commerce_platform/orders_controller.rb
+++ app/controllers/solidus_paypal_commerce_platform/orders_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module SolidusPaypalCommercePlatform
   class OrdersController < ::Spree::Api::BaseController
     skip_before_action :authenticate_user
